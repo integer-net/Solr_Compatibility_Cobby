2a6d6362a2bc51ddaec1039c6f3c4c3e927de8fd
Andreas von Studnitz <avs@integer-net.de>
2016-08-04 10:31:43 +0200

    Implement compatibility to cobby indexer

---
+++ src/app/code/community/IntegerNet/SolrCompatibilityCobby/Model/Observer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_SolrCompatibilityCobby
+ * @copyright  Copyright (c) 2016 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */
+class IntegerNet_SolrCompatibilityCobby_Model_Observer
+{
+    /**
+     * Reindexes IntegerNet_Solr after Cobby import
+     *
+     * @param Varien_Event_Observer $observer
+     * @event cobby_after_product_import
+     */
+    public function reindexSolrAfterCobbyImport(Varien_Event_Observer $observer)
+    {
+        /** @var $indexer Mage_Index_Model_Process */
+        $indexer = Mage::getModel('index/process')->load('integernet_solr', 'indexer_code');
+        if ($indexer->getMode() != Mage_Index_Model_Process::MODE_REAL_TIME) {
+            return;
+        }
+        
+        $productIds = array();
+        foreach ($observer->getEntities() as $sku => $productData) {
+            $productIds[] = $productData['entity_id'];
+        }
+        
+        if (empty($productIds)) {
+            return;
+        }
+        
+        Mage::helper('integernet_solr/factory')->getProductIndexer()->reindex($productIds);
+    }
+}
